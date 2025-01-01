f0843b409223ec72e7dd3d8ad37d8dfd65653d96
uyuryusuf <yusuf_uyur@hotmail.com>
2025-01-01 17:38:47 +0300

    proje yukleme

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+
+  if (name === "" && phone === "" && email === "") {
+      alert("Lütfen tüm alanları doldurun.");
+  } else {
+      alert("Mesaj başarıyla gönderildi.");
+      
+      document.getElementById('contact-form').reset();
+  }
+});
